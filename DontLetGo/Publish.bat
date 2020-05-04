f38cec567547e9afe5d399de726d0df03a269187
Ellpeck <megamaximal@gmail.com>
2020-05-05 01:30:24 +0200

    publish script

---
+++ DontLetGo/Publish.bat
@@ -0,0 +1,5 @@
+gamebundle -wlmt
+
+butler push bin/Bundled/win ellpeck/dont-wake-up:win --fix-permissions
+butler push bin/Bundled/linux ellpeck/dont-wake-up:linux --fix-permissions
+butler push bin/Bundled/mac ellpeck/dont-wake-up:mac --fix-permissions
