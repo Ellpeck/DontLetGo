1f6da60a997bb5b588b50d7d21039106cfbe8e10
Ellpeck <megamaximal@gmail.com>
2020-05-05 03:07:33 +0200

    don't trim pls

--- DontLetGo/Publish.bat
+++ DontLetGo/Publish.bat
@@ -1,4 +1,4 @@
-gamebundle -wlmt
+gamebundle -wlm
 
 butler push bin/Bundled/win ellpeck/dont-wake-up:win --fix-permissions
 butler push bin/Bundled/linux ellpeck/dont-wake-up:linux --fix-permissions
